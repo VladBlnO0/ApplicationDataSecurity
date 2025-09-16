d2013fca26c60ec298d9b404ce5ea97bedf1df23
VLBL <vladpuku@gmail.com>
2025-09-16 20:54:04 +0300

    syntaxes changes

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-P = 2
-Q = 11
+P=2
+Q=11
 e=7
 d=3
 alphabet="1234567890"
-N = P*Q
+N=P*Q
 euler_function=(P-1)*(Q-1)
 
 def en_de_crypt(text, exp):
-    list = []
+    ed_list = []
     for i in range(text.__len__()):
-        list.append(str((int(text[i]) ** exp) % N))
-    return list
+        ed_list.append(str((int(text[i]) ** exp) % N))
+    return ed_list
 
 def check_txt(org_txt, dec_txt):
     if org_txt == dec_txt:
